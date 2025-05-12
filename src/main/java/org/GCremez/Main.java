44068ea90e715cf8695c9836559845e85c889621
GCremez <salauolorunjuedalo@gmail.com>
2025-05-12 14:13:01 +0300

    Initial Commit

--- src/main/java/org/GCremez/Main.java
+++ src/main/java/org/GCremez/Main.java
@@ -1,8 +1,34 @@
 package org.GCremez;
 
 
+import org.GCremez.timer.PomodoroTimer;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PomodoroTimer pomodoro = new PomodoroTimer();
 
+        System.out.println("Welcome To Pomodoro Cli Timer");
+        System.out.println("Commands: Start | Pause | Resume | Stop | Exit");
+
+        while(true) {
+            System.out.println("> ");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            switch (input){
+                case "start" -> pomodoro.startSession();
+                case "pause" -> pomodoro.pauseSession();
+                case "resume" -> pomodoro.resumeSession();
+                case "stop" -> pomodoro.stopSession();
+                case "exit"-> {
+                    pomodoro.stopSession();
+                    System.out.println("Goodbye!");
+                    return;
+                }
+                default -> System.out.println("Unknown Command.");
+            }
+        }
     }
 }
--- src/main/java/org/GCremez/timer/PomodoroTimer.java
+++ src/main/java/org/GCremez/timer/PomodoroTimer.java
@@ -1,4 +1,31 @@
 package org.GCremez.timer;
 
+import java.util.Timer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class PomodoroTimer {
+    private Timer timer;
+    private int durationInSeconds;
+    private int timeleft;
+    private final AtomicBoolean isPaused = new AtomicBoolean(false);
+    private final AtomicBoolean isRunning = new AtomicBoolean(false);
+
+
+    public PomodoroTimer(){
+        this.durationInSeconds = 25 * 60; // default 25 mins
+        this.timeleft = durationInSeconds;
+    }
+
+
+    public void startSession() {
+    }
+
+    public void pauseSession() {
+    }
+
+    public void resumeSession() {
+    }
+
+    public void stopSession() {
+    }
 }
