3fea778008797472f54aac90527583cace1c1968
GCremez <salauolorunjuedalo@gmail.com>
2025-05-12 15:40:00 +0300

    CREATE SESSION

--- src/main/java/org/GCremez/timer/PomodoroTimer.java
+++ src/main/java/org/GCremez/timer/PomodoroTimer.java
@@ -1,6 +1,7 @@
 package org.GCremez.timer;
 
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PomodoroTimer {
@@ -18,9 +19,39 @@ public class PomodoroTimer {
 
 
     public void startSession() {
+        if (isRunning.get()) {
+            System.out.println("Timer is already running");
+            return;
+        }
+        isRunning.set(true);
+        timer = new Timer();
+        System.out.println("Starting Pomodoro Session: " + (timeleft / 60) + "minutes.");
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (isPaused.get()) return;
+
+                if (timeleft > 0) {
+                    int minutes = timeleft / 60;
+                    int seconds = timeleft % 60;
+                    System.out.printf(" %02d:%02d remaining\r", minutes, seconds);
+                    timeleft--;
+                } else {
+                    System.out.println("\n Session Complete!");
+                    stopSession();
+                }
+            }
+        }, 0, 1000);
     }
 
     public void pauseSession() {
+        if (!isRunning.get()) {
+            System.out.println("No Session to pause.");
+            return;
+        }
+        isPaused.set(true);
+        System.out.println("Timer Paused");
     }
 
     public void resumeSession() {
